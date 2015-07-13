619dbd15a73da44b2ac29a2975bebf81c6c8a4bf
Douglas Davis <ddavis@phy.duke.edu>
2015-07-13 15:10:57 -0400

    plots are looking good

--- plotting/FRZ_plot.py
+++ plotting/FRZ_plot.py
@@ -41,34 +41,35 @@ ratios = { h : in_file.Get(h+'_ratio')           for h in hists }
 for h in hists:
     for p in processes:
         cur_hist = in_file.Get(h+'_'+p)
-        cur_hist.GetXaxis().SetLabelOffset(99)
         if not p == 'data':
             cur_hist.SetFillColor(colors[p])
             cur_hist.SetLineColor(colors[p])
+            plot_utils.hist_formatting(cur_hist,'not_data')
             stacks[h].Add(cur_hist)
         else:
-            pass
+            plot_utils.hist_formatting(cur_hist,'data')
         if p == 'zjets':
             cur_hist.SetLineColor(ROOT.kBlack)
+            cur_hist.SetLineWidth(2)
 
 c_MET  = ROOT.TCanvas('c_MET','c_MET', 600,450)
-p1_MET = ROOT.TPad('p1_MET',  'p1_MET',0.05,0.31,0.95,0.95)
-p2_MET = ROOT.TPad('p2_MET',  'p2_MET',0.05,0.00,0.95,0.3)
+p1_MET = ROOT.TPad('p1_MET',  'p1_MET',0.0,0.31,0.95,0.95)
+p2_MET = ROOT.TPad('p2_MET',  'p2_MET',0.0,0.00,0.95,0.3)
 p1_MET.cd()
 in_file.Get('MET_data').Draw('e')
 stacks['MET'].Draw('same')
 plot_utils.legend.Draw('same')
 in_file.Get('MET_data').Draw('same,e')
-plot_utils.atlaslabel.DrawLatex(.515,.85,'ATLAS')
-plot_utils.atlaslabelText.DrawLatex(.515+0.130,.85,'Work in Progress')
+plot_utils.atlaslabel.DrawLatex(.2,.83,'ATLAS')
+plot_utils.atlaslabelText.DrawLatex(.2+0.110,.83,'Work in Progress, #sqrt{s} = 8 TeV, 20.3 fb^{-1}')
 p2_MET.cd()
 plot_utils.hist_ratio_formatting(ratios['MET'])
 ratios['MET'].Draw('e')
+plot_utils.line_at_one.Draw('same')
 plot_utils.pad_margining(p1_MET,p2_MET)
 c_MET.cd()
 p1_MET.Draw()
 p2_MET.Draw()
 p1_MET.RedrawAxis()
 p2_MET.RedrawAxis()
-
-raw_input('')
+c_MET.SaveAs(out_folder+'/MET.'+file_ext)
